df1864818b0082afe48d71e914e63c2903da1d68
mariahgc <maria.17aac16@gmail.com>
2025-04-29 08:55:37 -0300

    feat: inclusão do relacionamento de Veículo com Modelo e Cor.

--- core/models/veiculo.py
+++ core/models/veiculo.py
@@ -1,12 +1,9 @@
 from django.db import models
-
-
-
+from .cor import Cor
+from .modelo import Modelo
+ 
 class Veiculo(models.Model):
-     nome = models.CharField(max_length=80, blank=True, null=True)
-     marca = models.CharField(max_length=80, blank=True, null=True)
-     categoria = models.CharField(max_length=80, blank=True, null=True)
- 
- 
-     def __str__(self):
-         return f"({self.id}){self.nome.upper()}"
+     modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name="veiculo", null=True, blank=True)
+     cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculo", null=True, blank=True)
+     ano = models.IntegerField(null=True, default=0)
+     preco = models.DecimalField(max_digits=10,decimal_places=2, null=True, default=0)
