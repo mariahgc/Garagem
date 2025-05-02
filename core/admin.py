175e62a1f2e02c869d0011cda0b80819462e1e94
mariahgc <maria.17aac16@gmail.com>
2025-05-02 20:59:30 -0300

    feat: finalizando atividade

--- core/admin.py
+++ core/admin.py
@@ -6,6 +6,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
+from cffi import model
 from core import models
 
 
--- core/models/veiculo.py
+++ core/models/veiculo.py
@@ -1,9 +1,17 @@
 from django.db import models
 from .cor import Cor
 from .modelo import Modelo
+from .acessorio import Acessorio
+from core.models import Acessorio
  
 class Veiculo(models.Model):
      modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name="veiculo", null=True, blank=True)
      cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculo", null=True, blank=True)
      ano = models.IntegerField(null=True, default=0)
      preco = models.DecimalField(max_digits=10,decimal_places=2, null=True, default=0)
+     acessorio = models.ManyToManyField(Acessorio, related_name='veiculo', blank=True)
+
+
+
+    def __str__(self):
+        return f"{self.id}"
