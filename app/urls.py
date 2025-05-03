edae14c9f00487fa61151871611e466fd719620e
mariahgc <maria.17aac16@gmail.com>
2025-05-03 13:41:48 -0300

    Feat: arrumando erro

--- app/urls.py
+++ app/urls.py
@@ -8,17 +8,15 @@ from drf_spectacular.views import (
 )
 from rest_framework.routers import DefaultRouter
 
-from core.views import UserViewSet, VeiculoViewSet
-from core.views import AcessorioViewSet, CorViewSet, UserViewSet, ModeloViewSet
+from core.views import UserViewSet
+from core.views import AcessorioViewSet, CorViewSet, ModeloViewSet, UserViewSet, VeiculoViewSet
 
 router = DefaultRouter()
 
 router.register(r'usuarios', UserViewSet, basename='usuarios')
-router.register(r"acessorios", AcessorioViewSet, basename='acessorios')
+router.register(r"acessorio", AcessorioViewSet, basename='acessorios')
 router.register(r"cores", CorViewSet, basename='cores')
 router.register(r"modelos", CorViewSet, basename='modelos')
-router.register(r"veiculos", VeiculoViewSet, basename='veiculos')
-
 
 
 urlpatterns = [
--- core/models/veiculo.py
+++ core/models/veiculo.py
@@ -2,14 +2,14 @@ from django.db import models
 from .cor import Cor
 from .modelo import Modelo
 from .acessorio import Acessorio
-from core.models import Acessorio
- 
+from core.models import acessorio
+
 class Veiculo(models.Model):
-     modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name="veiculo", null=True, blank=True)
-     cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculo", null=True, blank=True)
-     ano = models.IntegerField(null=True, default=0)
-     preco = models.DecimalField(max_digits=10,decimal_places=2, null=True, default=0)
-     acessorio = models.ManyToManyField(Acessorio, related_name='veiculo', blank=True)
+    modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name="veiculo", null=True, blank=True)
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculo", null=True, blank=True)
+    ano = models.IntegerField(null=True, default=0)
+    preco = models.DecimalField(max_digits=10,decimal_places=2, null=True, default=0)
+    acessorio = models.ManyToManyField(Acessorio, related_name='veiculo', blank=True)
 
 
 
